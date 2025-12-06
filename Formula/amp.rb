9387d3196c4c92a35456108610984f705b1b2b7c
Tim Culverhouse <tim@timculverhouse.com>
2025-12-06 15:41:57 +0200

    Update amp to 0.0.1765028099-g363900

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -1,20 +1,21 @@
 class Amp < Formula
   desc "CLI for Amp, the frontier coding agent"
   homepage "https://ampcode.com/"
-  version "0.0.1749146413-gee6da4"
+  version "0.0.1765028099-g363900"
 
   url "https://registry.npmjs.org/@sourcegraph/amp/-/amp-#{version}.tgz"
-  sha256 "PLACEHOLDER"
+  sha256 "b235ab68aef00a41f0a04c30ce4da4cc6f60f9e5e6d98152596d6f9201d5a9d6"
   license :cannot_represent
+
+  livecheck do
+    url "https://registry.npmjs.org/@sourcegraph/amp"
+    regex(/"version":\s*"([^"]+)"/i)
+  end
 
   depends_on "node@20"
 
   def install
     system "npm", "install", "--prefix", libexec, "."
     bin.install_symlink libexec/"node_modules/.bin/amp"
-  end
-
-  test do
-    assert_match version.to_s, shell_output("#{bin}/amp --version")
   end
 end
