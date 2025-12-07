3fc359a50ae1403f6900a64ed96a840e8e81831c
Tim Culverhouse <tim@timculverhouse.com>
2025-12-07 09:00:29 +0200

    Update amp to 0.0.1765090420-gae83ff

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -1,10 +1,10 @@
 class Amp < Formula
   desc "CLI for Amp, the frontier coding agent"
   homepage "https://ampcode.com/"
-  version "0.0.1765065699-g62535d"
+  version "0.0.1765090420-gae83ff"
 
   url "https://registry.npmjs.org/@sourcegraph/amp/-/amp-#{version}.tgz"
-  sha256 "a987ea77340c8659fe30b8dba0cdd66b736e5e4bfb431e14536a933926de2a30"
+  sha256 "594d0dbcc8be2903224d9084c42ff22825426447ced3e1bb4d8730cedb6fdda7"
   license :cannot_represent
 
   livecheck do
