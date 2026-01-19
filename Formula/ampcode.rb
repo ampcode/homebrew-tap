323d19a902a6ff4a8ebbb9a083bf4fabfbfb6bc6
Tim Culverhouse <tim@timculverhouse.com>
2026-01-19 06:08:38 -0600

    Update ampcode to 0.0.1768824105-g56038a

--- Formula/ampcode.rb
+++ Formula/ampcode.rb
@@ -1,10 +1,10 @@
 class Ampcode < Formula
   desc "CLI for Amp, the frontier coding agent"
   homepage "https://ampcode.com/"
-  version "0.0.1768809715-gc19c40"
+  version "0.0.1768824105-g56038a"
 
   url "https://registry.npmjs.org/@sourcegraph/amp/-/amp-#{version}.tgz"
-  sha256 "fbf0e202d386ed48eaec40f8c232c00516ef4fe8d1734d3c70a8958d0c2c0109"
+  sha256 "76117e2854d8da66346c50f9d66fdbd069f61806f0b2a453fa96111b1c733ad0"
   license :cannot_represent
 
   livecheck do
