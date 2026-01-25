3738c62a4b20048d9b593842c4ece0c46e362df4
Tim Culverhouse <tim@timculverhouse.com>
2026-01-25 09:01:59 -0600

    Update ampcode to 0.0.1769352912-g256385

--- Formula/ampcode.rb
+++ Formula/ampcode.rb
@@ -1,21 +1,35 @@
 class Ampcode < Formula
   desc "CLI for Amp, the frontier coding agent"
   homepage "https://ampcode.com/"
-  version "0.0.1769346334-gc7e300"
-
-  url "https://registry.npmjs.org/@sourcegraph/amp/-/amp-#{version}.tgz"
-  sha256 "bfa260ab4e1c201c0f5d248275883af39d3a88dc64ed3e691ebe73433cf6482a"
+  version "0.0.1769352912-g256385"
   license :cannot_represent
 
   livecheck do
-    url "https://registry.npmjs.org/@sourcegraph/amp"
-    regex(/"version":\s*"([^"]+)"/i)
+    url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/cli-version.txt"
+    regex(/^(.+)$/i)
   end
 
-  depends_on "node@20"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1769352912-g256385/amp-darwin-arm64"
+      sha256 "83737c3444a6581041c302820deb677d4f4da7109b24e5adaf0044c65d844146"
+    else
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1769352912-g256385/amp-darwin-x64"
+      sha256 "6b7361ab3c72e1045d6ee74f0df428d515cf6bb64dc3af2d7f54bb85e959cfe2"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1769352912-g256385/amp-linux-arm64"
+      sha256 "f462fce08795dafbfb031d8b4972dfaa28ba9b1de0a9975c68ad83408e86264f"
+    else
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1769352912-g256385/amp-linux-x64"
+      sha256 "f555b06aedbb8405ea1d9f749e2fc187db5d6bf9911729b2ef332ef68984610f"
+    end
+  end
 
   def install
-    system "npm", "install", "--prefix", libexec, "."
-    bin.install_symlink libexec/"node_modules/.bin/amp"
+    bin.install Dir["amp-*"].first => "amp"
   end
 end
