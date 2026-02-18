52eac0c6ea33389473326f034aa83fe21c98430c
Tim Culverhouse <tim@timculverhouse.com>
2026-02-17 18:12:50 -0600

    Update ampcode to 0.0.1771373086-gedddec

--- Formula/ampcode.rb
+++ Formula/ampcode.rb
@@ -1,7 +1,7 @@
 class Ampcode < Formula
   desc "CLI for Amp, the frontier coding agent"
   homepage "https://ampcode.com/"
-  version "0.0.1771358819-g9e835e"
+  version "0.0.1771373086-gedddec"
   license :cannot_represent
 
   livecheck do
@@ -11,21 +11,21 @@ class Ampcode < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1771358819-g9e835e/amp-darwin-arm64"
-      sha256 "681714dedb5ff3f7363c421ac4b0f27bf10b4bbec01d466b27bb1969dd56f1a7"
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1771373086-gedddec/amp-darwin-arm64"
+      sha256 "5ce86edc2adbda35684ac5eba7f0fc03168f2a4e8a9a2b4971a467de32575201"
     else
-      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1771358819-g9e835e/amp-darwin-x64"
-      sha256 "890d34602dce807dde289ed6c05f719769500f84d9d89389a397ce168a655999"
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1771373086-gedddec/amp-darwin-x64"
+      sha256 "810e090374686537ede0e8bc164f94a2ddcca78669f3f5ad581a683381ddb6fe"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1771358819-g9e835e/amp-linux-arm64"
-      sha256 "6889f8f5e9295992e6f20a5cbd1d4b0c4ef36f0eabde05ff59ba6e4eadc59f1c"
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1771373086-gedddec/amp-linux-arm64"
+      sha256 "7358da9d76de7272cbb7752c9a84d8f6196b9a991df09a605f14f99808221c84"
     else
-      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1771358819-g9e835e/amp-linux-x64"
-      sha256 "73c05d86c342f368cbab868042aa6ed6a12be69fa0a8b0baf60497a588d9a2b6"
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1771373086-gedddec/amp-linux-x64"
+      sha256 "de2b12a738da9f31285084518eb6b01b7a8f9f74f22b86f09e85791d40525fea"
     end
   end
 
