ba644c143ad7ce70ceaf4c85a165ebb628869181
Tim Culverhouse <tim@timculverhouse.com>
2026-03-12 23:38:11 -0500

    Update ampcode to 0.0.1773376232-g868261

--- Formula/ampcode.rb
+++ Formula/ampcode.rb
@@ -1,7 +1,7 @@
 class Ampcode < Formula
   desc "CLI for Amp, the frontier coding agent"
   homepage "https://ampcode.com/"
-  version "0.0.1773360219-g15db7c"
+  version "0.0.1773376232-g868261"
   license :cannot_represent
 
   livecheck do
@@ -11,21 +11,21 @@ class Ampcode < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1773360219-g15db7c/amp-darwin-arm64"
-      sha256 "835a1a213f6ad422f812d61f77f541f463d8fac33272b0b2da957a96f1f34bf6"
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1773376232-g868261/amp-darwin-arm64"
+      sha256 "fa73194625cf4fbc23129b7ed50d83bbaa1ff695f8e624bab33cf77d4dcd5e0e"
     else
-      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1773360219-g15db7c/amp-darwin-x64"
-      sha256 "61298f20808981a36cb4cca7f9cb0a171c4c7a4c6d4ee0139ce4a994dfe18977"
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1773376232-g868261/amp-darwin-x64"
+      sha256 "19cb6c05386938bf7b0314eb2e6edb2df78b5cdc367a9ac72e913505db59f588"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1773360219-g15db7c/amp-linux-arm64"
-      sha256 "9819104f44d73a2a06245107f6c2aee6539ac39d96cac0360a753b5c9551889a"
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1773376232-g868261/amp-linux-arm64"
+      sha256 "17609f6acf87086b71e2326b565cc36618c93878929bc4bd30b648a412276ead"
     else
-      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1773360219-g15db7c/amp-linux-x64"
-      sha256 "53a18e6c75c787add26f34a27eae9b028e2670d6df672449b1aa15d2321814aa"
+      url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1773376232-g868261/amp-linux-x64"
+      sha256 "d96cdfd6edbef8e1256e0780750d0f5c211e243ac259b444a55969b5b8216dbe"
     end
   end
 
